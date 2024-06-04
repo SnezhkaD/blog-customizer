85b0c397610bc8719f9df17342c1423aa25a28ee
Snezhana Dyachenko <Snezhka.1988@yandex.ru>
2024-06-04 20:41:02 +0300

    Commit message

--- src/components/arrow-button/ArrowButton.stories.tsx
+++ src/components/arrow-button/ArrowButton.stories.tsx
@@ -2,18 +2,26 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 import { ArrowButton } from './ArrowButton';
 
+import { useState } from 'react';
+
 const meta: Meta<typeof ArrowButton> = {
 	component: ArrowButton,
 };
 
 export default meta;
 type Story = StoryObj<typeof ArrowButton>;
+const [isActive, setIsActive] = useState(false);
+
+const handleClick = () => {
+	setIsActive((prev) => !prev);
+	alert('клик на кнопку');
+};
 
 export const ArrowButtonStory: Story = {
 	render: () => {
 		return (
 			<>
-				<ArrowButton />
+				<ArrowButton handleClick={handleClick} state={isActive} />
 			</>
 		);
 	},
--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -2,18 +2,36 @@ import arrow from 'src/images/arrow.svg';
 
 import styles from './ArrowButton.module.scss';
 
+import clsx from 'clsx';
+
 /** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
 
-export const ArrowButton = () => {
+interface ArrowButtonProps {
+	handleClick: OnClick;
+	state: boolean;
+}
+
+export const ArrowButton = ({ handleClick, state }: ArrowButtonProps) => {
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}>
-			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
+			onClick={handleClick}
+			className={clsx({
+				[styles.container]: true,
+				[styles.container_open]: state,
+			})}>
+			<img
+				src={arrow}
+				alt='иконка стрелочки'
+				className={clsx({
+					[styles.arrow]: true,
+					[styles.arrow_open]: state,
+				})}
+			/>
 		</div>
 	);
 };
